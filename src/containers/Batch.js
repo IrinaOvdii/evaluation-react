1ed93d78b05e58b11b75e2e12e2bc0d6dfd76ede
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-25 17:05:18 +0100

    Ask student algorithm

--- src/containers/Batch.js
+++ src/containers/Batch.js
@@ -47,79 +47,83 @@ class Batch extends PureComponent {
       }),
     }
 
-    componentWillMount() {
-        const { batch, fetchOneBatch, subscribeToWebsocket } = this.props;
-        const { batchId } = this.props.match.params;
+  componentWillMount() {
+      const { batch, fetchOneBatch, subscribeToWebsocket } = this.props;
+      const { batchId } = this.props.match.params;
 
-        if (!batch) {
-            fetchOneBatch(batchId)
-        }
-        subscribeToWebsocket()
-    }
-
-    componentWillReceiveProps(nextProps) {
-        const { batch } = nextProps;
-
-        if (batch && !batch.students[0].name) {
-            this.props.fetchStudents(batch)
-        }
-    }
-
-    goToStudent = (student, batchId) => () => {
-      this.props.push(`/batches/${batchId}/students/${student._id}`)
-    }
-
-    // onGetStudent = () => {
-    //   const names = this.props.batch ? this.props.batch.students.map(item => item.name) : []
-    //   const randName = names[Math.floor(Math.random()*names.length)]
-    //   alert(randName)
-    // }
-
-    onGetStudent = () => {
-      const studentsRed = this.props.batch.students.filter(i => i.lastColor === 'Red')
-      const studentsYellow = this.props.batch.students.filter(i => i.lastColor === 'Yellow')
-      const studentsGreen = this.props.batch.students.filter(i => i.lastColor === 'Green')
-      const randNumber = Math.floor((Math.random() * 100) + 1)
-      console.log(randNumber)
-
-      if(randNumber <= 49) {
-        var student = studentsRed[Math.floor(Math.random()*studentsRed.length)]
+      if (!batch) {
+          fetchOneBatch(batchId)
       }
-      else if ((randNumber > 49) && (randNumber <= 82)) {
-        var student = studentsYellow[Math.floor(Math.random()*studentsYellow.length)]
-      }
-      else if (randNumber > 82) {
-        var student = studentsGreen[Math.floor(Math.random()*studentsGreen.length)]
-      }
-
-      alert(`${student.name}`)
-    }
-
-    render() {
-      const { batch } = this.props
-      if (!batch) return null
-      const { _id } = batch
-
-      return (
-        <div style={styles.batchContainer} className="Batch">
-          {batch.students.map(student => (
-            <div
-              style={styles.studentContainer}
-              key={student._id}
-              onClick={this.goToStudent(student, _id)} >
-
-              <h5>{student.name}</h5>
-              <img src={student.photo} alt={student.name} width='20%'/>
-              <h6>{student.lastColor}</h6>
-
-            </div>))}
-            <div>
-              <button onClick={this.onGetStudent}>ASK A QUESTION</button>
-            </div> <br/>
-            < StudentEditor {...this.props}/>
-        </div>)
-    }
+      subscribeToWebsocket()
   }
+
+  componentWillReceiveProps(nextProps) {
+      const { batch } = nextProps;
+
+      if (batch && !batch.students[0].name) {
+          this.props.fetchStudents(batch)
+      }
+  }
+
+  goToStudent = (student, batchId) => () => {
+    this.props.push(`/batches/${batchId}/students/${student._id}`)
+  }
+
+  getRandomizedArray(items, amount) {
+    if(0 === items.length){
+      return []
+    }
+
+    var result = []
+    for(var i = 0; i < amount; i++){
+      result.push(items[Math.floor(Math.random() * items.length)]);
+    }
+    return result
+  }
+
+  onGetStudent = () => {
+    const studentsRed = this.props.batch.students.filter(i => i.lastColor === 'Red');
+    const studentsYellow = this.props.batch.students.filter(i => i.lastColor === 'Yellow');
+    const studentsGreen = this.props.batch.students.filter(i => i.lastColor === 'Green');
+
+    var randomizedStudents = []
+      randomizedStudents = randomizedStudents.concat(
+        this.getRandomizedArray(studentsGreen, 18),
+        this.getRandomizedArray(studentsRed, 49),
+        this.getRandomizedArray(studentsYellow, 33)
+      )
+    var index = Math.floor(Math.random()*randomizedStudents.length)
+    var student = randomizedStudents[index];
+    console.log(index)
+
+    alert(`Question for ${student.name} with last color ${student.lastColor}`)
+  }
+
+  render() {
+    const { batch } = this.props
+    if (!batch) return null
+    const { _id } = batch
+
+    return (
+      <div style={styles.batchContainer} className="Batch">
+        {batch.students.map(student => (
+          <div
+            style={styles.studentContainer}
+            key={student._id}
+            onClick={this.goToStudent(student, _id)} >
+
+            <h5>{student.name}</h5>
+            <img src={student.photo} alt={student.name} width='20%'/>
+            <h6>{student.lastColor}</h6>
+
+          </div>))}
+          <div>
+            <button onClick={this.onGetStudent}>ASK A QUESTION</button>
+          </div> <br/>
+          < StudentEditor {...this.props}/>
+      </div>)
+  }
+}
 
 const mapStateToProps = ({ batches }, { match }) => {
     const batch = batches.filter((g) => (g._id === match.params.batchId))[0];
